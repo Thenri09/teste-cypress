aefba8fb9500fb7f3ba7e73f9776bbc61a20ef68
Thiago Henrique <thghenrique26@gmail.com>
2024-10-04 12:17:59 -0300

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,28 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
    
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Argus All-Weather Tank')
-        .click()
+    it.('Deve selecionar um produto da lista', () => {
+       produtosPage.buscarProdutosLista('Aether Gym Pant')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Apollo Running Short'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página do produtos', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
